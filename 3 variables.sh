031a3be8f8b31e8c250b4671eaca05c0f279b7fd
ramya <ramya.pekatanoju@gmail.comgit config --global user.name ramyawqwq!clearqexitcleargit config --global user[200~git config --global user.name Mona>
2024-05-13 00:04:03 +0530

    hello shell-script

---
+++ 3 variables.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+
+#decalring of variable
+
+here we specicfy the person with $ symbol
+her $=varaiable
+PERSON1=venu  no space between = value
+person2=ramya
+
+#refereing of varaiable
+echo hi $PERSON1 how is your work
+echo hello $PERSON2 iam doing well. what about your work.
+echo my work is going very well $PERSON2.
+echo ok $PERSON1 bye.
