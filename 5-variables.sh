c38ca81be333f7b2a02b0e43d8808cd4eb2a31ca
ramya <ramya.pekatanoju@gmail.com>
2024-05-15 00:37:14 +0530

    here declaring the variables

--- 5-variables.sh
+++ 5-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "please enter your username::"
-read USERNAME #username is variable here and her read stores the value of echo 
+read -s USERNAME #username is variable here and her read stores the value of echo 
 echo "please enter your password::"
-read PASSWORD
+read -s PASSWORD
 echo username is : $USERNAME, password is :$PASSWORD
