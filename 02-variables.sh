5baf8547ebab470b43508b08163aef765f04db03
ramya <ramya.pekatanoju@gmail.com>
2024-05-15 00:17:39 +0530

    adding some message in to variables

--- 02-variables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-echo "ramesh"::hello suresh how are you?
-echo "suresh"::hi iam fine .how are you ?
-echo "Ramesh":: Iam fine to ,how is your work?
-echo "suresh":: Iam not bad .iam thinking to  upgrade to Devops
---
+++ 2-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh":: Hi suresh how r u ?
+echo "suresh" :: hi ramesh iam goog . how about you?
+echo "ramesh" :: all good how was your work ?
+echo "suresh" :: ha ramesh i want to upgrade to Devops.
