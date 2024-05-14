3f435700e318e0fe42c3b571f489ec9f63276cb5
ramya <ramya.pekatanoju@gmail.com>
2024-05-15 00:44:37 +0530

    here declaring the variables

---
+++ array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MOVIES=("RRR" "LOVER" "ONE")
+#size of above array is 3
+#index of above array is 0,1,2
+# list always start with 0
+
+echo "First movie is :${MOVIES[0]}"
+echo "Second movie is :${MOVIES[1]}"
+echo "third movie is :${MOVIES[2]}"
