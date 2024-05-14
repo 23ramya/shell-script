ce6b43d8071e89dd274fa920f781968ab9924395
ramya <ramya.pekatanoju@gmail.com>
2024-05-14 22:50:49 +0530

    add variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,15 +3,16 @@
 
 #decalring of variable
 
-here we specicfy the person with $ symbol
-her $=varaiable
+#here we specicfy the person with $ symbol
+#here $=varaiable
 PERSON1=venu  #no space between = and value
 PERSON2=ramya
 
 #refereing of varaiable
 
-echo $PERSON1 hello suresh how are you?
-echo $PERSON2 hi iam fine .how are you ?
-echo $PERSON1 Iam fine to ,how is your work?
-echo $PERSON1  Iam not bad .iam thinking to  upgrade to Devops
+echo "$PERSON1::hello $PERSON2 how are you?"
+echo "$PERSON2::hi iam fine .how are you ?"
+echo "$PERSON1::Iam fine to ,how is your work?"
+echo "$PERSON1::Iam not bad .iam thinking to  upgrade to Devops"
 
+ 
