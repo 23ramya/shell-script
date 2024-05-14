a98c07148756e4467dfa267c8f32076b02906a12
ramya <ramya.pekatanoju@gmail.com>
2024-05-15 00:27:35 +0530

    here declaring the variables

---
+++ 4-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1":: Hi $PERSON2 how r u ?
+echo "$PERSON2" :: hi $PERSON1 iam goog . how about you?
+echo "$PERSON1" :: all good how was your work ?
+echo "$PERSON1" :: ha $PERSON1 i want to upgrade to Devops.
