bd12a41d7d32e856d37b191f0ed448cbf8f5c3d0
ramya <ramya.pekatanoju@gmail.com>
2024-05-15 01:20:10 +0530

    here declaring the variables

--- conditions.sh
+++ conditions.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
  NUMBER=$1
+
 if [$NUMBER -gt 0]
+
  then
  echo "Given number $NUMBER  greater than 10"
+
  else
  echo "Given number $NUMBER less than 10"
+ 
  Fi
 
  #-git=greaterthan= , -lt=lessthan= , -eq==  -le
