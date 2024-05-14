bd9ee02a030ed89eaca924cdd0598726c6f09a6f
ramya <ramya.pekatanoju@gmail.com>
2024-05-15 00:34:27 +0530

    here declaring the variables

--- 4-variables.sh
+++ 4-variables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#declaring of a variable
 PERSON1=$1
 PERSON2=$2
-
+#refering of a variable
 echo "$PERSON1":: Hi $PERSON2 how r u ?
 echo "$PERSON2" :: hi $PERSON1 iam goog . how about you?
 echo "$PERSON1" :: all good how was your work ?
---
+++ 5-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "please enter your username::"
+read USERNAME #username is variable here and her read stores the value of echo 
+echo "please enter your password::"
+read PASSWORD
+echo username is : $USERNAME, password is :$PASSWORD
