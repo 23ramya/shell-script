bce0f7b15e1cd00ca665bcb168b6c22a717e5c20
ramya <ramya.pekatanoju@gmail.com>
2024-05-15 23:04:08 +0530

    here declaring the variables

--- installpackages.sh
+++ installpackages.sh
@@ -6,39 +6,37 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
- 
- VALIDATE(){
- if [ $1 -ne 0]
-    then
-    echo -e "$2....$R FALURE $N"
-    exit1
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
     else
-     echo -e "$2....$G SUCEESS $N"
+        echo -e "$2...$G SUCCESS $N"
     fi
- }
- if [ $USERID -ne 0 ]
- then
+}
 
- echo "please run this script with root access"
- exit 1
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
 
- else 
-
- echo "your are super user"
- fi
-
-
-  for i in $@
-  do
-  echo "packages to install: $i"
-  dnf  list installed $i &>>LOGFILE
-    if[ $? -eq  0 ]
-   
+for i in $@
+do
+    echo "package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
     then
-    echo   -e "$i already installed...$Y SKIPPING $N"
+        echo -e "$i already installed...$Y SKIPPING $N"
     else
-    dnf install $i -y &>>LOGFILE
-    VALIDATE $? :installation of $i"
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
     fi
-    done
+done
+
