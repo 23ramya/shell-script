472a9fd1571707e873c576dd660f3161bd954633
ramya <ramya.pekatanoju@gmail.com>
2024-05-15 23:03:26 +0530

    here declaring the variables

--- installpackages.sh
+++ installpackages.sh
@@ -34,6 +34,7 @@ N="\e[0m"
   echo "packages to install: $i"
   dnf  list installed $i &>>LOGFILE
     if[ $? -eq  0 ]
+   
     then
     echo   -e "$i already installed...$Y SKIPPING $N"
     else
